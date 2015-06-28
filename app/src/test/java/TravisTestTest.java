32a510360f0561cd636bcde3277880f7a8c60cce
MDP <mike@sdpdigital.com>
2015-06-28 17:08:17 -0500

    added junit test to see if travis ci can handle it

---
+++ app/src/test/java/TravisTestTest.java
@@ -0,0 +1,20 @@
+import junit.framework.TestCase;
+
+/**
+ * Created by TheMDP on 6/28/15.
+ */
+public class TravisTestTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void test_case() {
+        assertEquals(true, true);
+    }
+}
