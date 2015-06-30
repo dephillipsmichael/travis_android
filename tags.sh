8fc55dea54a8289d77e83b976cc360d51f109a42
MDP <mike@sdpdigital.com>
2015-06-29 19:40:36 -0500

    updated with tag to test

--- tags.sh
+++ tags.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "Looking for git tag $TRAVIS_TAG to contain the word \"tablet\" to do deployment"
-if[[ "$TRAVIS_TAG" =~ "tablet" ]]; then
+if [[ "$TRAVIS_TAG" =~ "tablet" ]]; then
   echo "Valid tag"
 else
   echo "invalid tag"
 fi
+echo "done"
